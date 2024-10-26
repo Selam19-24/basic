9dcdf1bcd450252552d3e264fac4de654a4debb8
Selam19-24 <selambenselam19@gmail.com>
2024-10-27 02:06:08 +0300

    Create a.jsx

---
+++ a.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Merhaba Dünya</h1>
+        <p>Bu bir basit React uygulamasıdır.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
